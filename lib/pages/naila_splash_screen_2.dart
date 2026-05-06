d270f055c12c5ccfca84584998335c378815d66d
ALEN <rohmatulumminaila@gmail.com>
2026-05-06 11:05:12 +0700

    pekerjaan naila

---
+++ lib/pages/naila_splash_screen_2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NailaSplashScreen2 extends StatefulWidget {
+  const NailaSplashScreen2({super.key});
+
+  @override
+  State<NailaSplashScreen2> createState() => _NailaSplashScreen2State();
+}
+
+class _NailaSplashScreen2State extends State<NailaSplashScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
