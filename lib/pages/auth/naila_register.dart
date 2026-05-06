6b5dc24cd2f71dccdd43d80c49b22a7a5fb2a66a
ALEN <rohmatulumminaila@gmail.com>
2026-05-06 11:24:06 +0700

    pekerjaan naila

---
+++ lib/pages/auth/naila_register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NailaRegister extends StatefulWidget {
+  const NailaRegister({super.key});
+
+  @override
+  State<NailaRegister> createState() => _NailaRegisterState();
+}
+
+class _NailaRegisterState extends State<NailaRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
