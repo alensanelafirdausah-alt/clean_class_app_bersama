a3c1bf5dfc4fdc6ad94c1e9dbd3e59c4d3e264a6
ALEN <rohmatulumminaila@gmail.com>
2026-05-06 11:26:04 +0700

    pekerjaan naila

---
+++ lib/pages/auth/alensa_login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlensaLogin extends StatefulWidget {
+  const AlensaLogin({super.key});
+
+  @override
+  State<AlensaLogin> createState() => _AlensaLoginState();
+}
+
+class _AlensaLoginState extends State<AlensaLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
