c3c531b74c71f124cbb0ba277a0e70f70ba00db6
User <alensanelafirdausah@gmail.com>
2026-05-05 23:08:22 -0700

    selesai

--- lib/pages/naila_splash_screen_2.dart
+++ lib/pages/naila_splash_screen_2.dart
@@ -10,6 +10,62 @@ class NailaSplashScreen2 extends StatefulWidget {
 class _NailaSplashScreen2State extends State<NailaSplashScreen2> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.orangeAccent.shade100,
+      body: Column(
+        children: [
+          Container(
+            width: size.width,
+            height: size.height * 0.55,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(0),
+                bottomRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/images/iconclean.png',
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            'Clean Class App',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          Text(
+            'Better teamwork for a better room',
+            style: TextStyle(fontSize: 30, color: Colors.black),
+          ),
+          SizedBox(
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Next',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
