b5ccc58b79b57a7e28b4a8d5b1df8953af9c865d
User <alensanelafirdausah@gmail.com>
2026-05-06 06:08:34 -0700

    horeeee

---
+++ lib/pages/bareng_menu_utama.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BarengMenuUtama extends StatefulWidget {
+  const BarengMenuUtama({super.key});
+
+  @override
+  State<BarengMenuUtama> createState() => _BarengMenuUtamaState();
+}
+
+class _BarengMenuUtamaState extends State<BarengMenuUtama> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
