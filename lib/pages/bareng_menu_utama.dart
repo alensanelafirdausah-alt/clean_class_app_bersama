86278f76a7ad86d88d541512f99522363b36bfbb
User <alensanelafirdausah@gmail.com>
2026-05-09 05:40:37 -0700

    horeeee

--- lib/pages/bareng_menu_utama.dart
+++ lib/pages/bareng_menu_utama.dart
@@ -10,6 +10,33 @@ class BarengMenuUtama extends StatefulWidget {
 class _BarengMenuUtamaState extends State<BarengMenuUtama> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                ),
+              ),
+              child: Container(
+                height: 20,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20)
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
