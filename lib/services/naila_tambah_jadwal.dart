0b97cf66c45f5b699e55e8563d160bac2cb52859
User <alensanelafirdausah@gmail.com>
2026-05-19 05:03:12 -0700

    horeeee

---
+++ lib/services/naila_tambah_jadwal.dart
@@ -0,0 +1,19 @@
+import 'package:clean_class_app_bersama/models/naila_modeks_jadwal.dart';
+import 'package:flutter/material.dart';
+
+List<NailaModelsJadwal> arr = [
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+  NailaModelsJadwal(icon: Icons.check_box, name: "Alensa"),
+];
